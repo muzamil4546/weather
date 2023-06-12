a2e2bd40c6c0f320d6961dcc10a8449c6bf12a6c
muzamil4546 <muzamilchamp14@gmail.com>
2023-06-12 11:14:25 -0700

    Cities added

---
+++ lib/ui_screens/get_started.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:weather/models/constants.dart';
+import 'package:weather/ui_screens/welcome.dart';
+
+class GetStarted extends StatefulWidget {
+  const GetStarted({super.key});
+
+  @override
+  State<GetStarted> createState() => _GetStartedState();
+}
+
+class _GetStartedState extends State<GetStarted> {
+  @override
+  Widget build(BuildContext context) {
+
+    Constants myConstants = Constants();
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: myConstants.primaryColor.withOpacity(.6),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("assets/get-started.png"),
+              SizedBox(height: 50),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(
+                          builder: (context)=> Welcome()));
+                },
+                child: Container(
+                  height: 50,
+                  width: size.width * 0.7,
+                  decoration: BoxDecoration(
+                    color: myConstants.primaryColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: Text('Get Started',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20)
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui_screens/welcome.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:weather/models/city.dart';
+import 'package:weather/models/constants.dart';
+import 'dart:ui';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    List<City> cities = City.citiesList.where((city) => city.isDefault == false).toList();
+    List<City> selectedCities = City.getSelectedCities();
+
+    Constants myConstants = Constants();
+
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: myConstants.secondaryColor,
+        centerTitle: true,
+        title: Text(selectedCities.length.toString() + ' Selected'),
+      ),
+      body: ListView.builder(
+        itemCount: cities.length,
+        itemBuilder: (BuildContext context, int index){
+          return Container(
+            margin: EdgeInsets.only(left: 10, top: 20, right: 10),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            height: size.height * .08,
+            width: size.width,
+            decoration: BoxDecoration(
+                border: cities[index].isSelected == true ? Border.all(
+                  color: myConstants.secondaryColor.withOpacity(.6),
+                  width: 2,
+                ) : Border.all(color: Colors.white),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  color: myConstants.primaryColor.withOpacity(.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                )
+              ]
+            ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      cities[index].isSelected =! cities[index].isSelected;
+                    });
+                  },
+                  child: Image.asset(cities[index]
+                      .isSelected == true ?
+                  'assets/checked.png' : 'assets/unchecked.png',
+                    width: 30,
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Text(cities[index].city,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: cities[index]
+                          .isSelected == true ?
+                      myConstants.secondaryColor: Colors.black54,
+                  )
+                )
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: myConstants.secondaryColor,
+        child: Icon(Icons.pin_drop),
+        onPressed: (){
+          print(selectedCities.length);
+        },
+      ),
+    );
+  }
+}
